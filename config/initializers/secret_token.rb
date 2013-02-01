0ce3b9afcfbdf4ad02322239370bb572193da753
Rifu <kuya.vinchi@gmail.com>
2013-01-31 19:13:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruuri::Application.config.secret_token = '161530063ab7b8f274fab1feb168fa30b3864fb8c48a2914d5073e948f48dbce2d8ad3cdd05f4b924410599c2492ea26a844c4ef6ee1e605a0202d299279dde0'
